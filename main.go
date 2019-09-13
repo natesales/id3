bf8bc46286e7da981ee38caf519053d27cc93726
Nate Sales <nate.wsales@gmail.com>
2019-09-13 15:01:09 -0700

    Change structure to map and fix Entropy function

--- main.go
+++ main.go
@@ -6,19 +6,20 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"math"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 func handle(e error) {
-	/**
-	 * handle
+
+	/* handle
 	 * Description: Handle an error. Well actually just panic and quit.
 	 */
+
 	if e != nil {
 		panic(e)
 	}
@@ -26,17 +27,16 @@ func handle(e error) {
 
 func readDataSet(filename string) ([]map[string]string, []map[string]string) {
 
-	/**
-	 * readDataSet
+	/* readDataSet
 	 * Description: Read data from text file randomly into slices of Entry objects.
 	 * filename: string Path to text file for reading
-	 * returns 2 []map[string]string objects containing random values from filename
+	 * returns: 2 []map[string]string objects containing random values from filename
 	 */
 
 	var training []map[string]string
 	var testing []map[string]string
 
-	r := rand.New(rand.NewSource(time.Now().Unix())) // Not a good seed. TODO
+	r := rand.New(rand.NewSource(time.Now().Unix())) // Not a good seed.
 
 	file, err := os.Open(filename)
 	handle(err)
@@ -45,23 +45,23 @@ func readDataSet(filename string) ([]map[string]string, []map[string]string) {
 		handle(err)
 	}()
 
-	first := true
+	var header []string
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		if first { // Skip the first line since it contains the header. TODO: Dont use struct, use a map
-			first = false
+		entry := strings.Split(scanner.Text(), ",")
+
+		if len(header) == 0 { // Skip the first line since it contains the header.
+			header = entry
 		} else {
-			entry := strings.Split(scanner.Text(), ",")
-			newEntry := map[string]string{
-				"PlayTennis":  entry[0],
-				"Outlook":     entry[1],
-				"Temperature": entry[2],
-				"Humidity":    entry[3],
-				"Wind":        entry[4],
+			newEntry := make(map[string]string)
+
+			for i, item := range header {
+				//fmt.Println("Assigning " + item + " to " + entry[i])
+				newEntry[item] = entry[i]
 			}
 
-			if r.Intn(2) == 1 {
+			if r.Intn(2) == 1 { // Split it up in roughly half
 				training = append(training, newEntry)
 			} else {
 				testing = append(testing, newEntry)
@@ -73,45 +73,83 @@ func readDataSet(filename string) ([]map[string]string, []map[string]string) {
 	return training, testing
 }
 
-func Entropy(entries []map[string]string) float64 { // TODO: Should this be float64? Its what Log2 returns.
+func Entropy(entries []map[string]string) float64 {
+
+	/* Entropy
+	 * Description: Calculate entropy for a given array of maps.
+	 * entries: Array of maps containing all the entries
+	 * returns: float64, entropy value
+	 */
+
 	var pYes float64
 	var pNo float64
-	for _, entry := range entries {
-		if entry["PlayTennis"] == "yes" {
+	for i, entry := range entries {
+		if entry["play tennis"] == "yes" {
 			pYes++
-		} else if entry["PlayTennis"] == "no" {
+		} else if entry["play tennis"] == "no" {
 			pNo++
 		} else {
-			log.Fatalf("PlayTennis is neither yes or no. It's " + entry["PlayTennis"])
+			fmt.Println(entry)
+			fmt.Println("PlayTennis is neither yes or no. It's '" + entry["PlayTennis"] + "' with index " + strconv.Itoa(i))
+			os.Exit(1)
 		}
 	}
-	pYes /= pYes + pNo
-	pNo /= pYes + pNo
+
+	total := pYes + pNo
+	pYes /= total
+	pNo /= total
 	return -(pYes * math.Log2(pYes)) - (pNo * math.Log2(pNo))
 }
 
-//func Gain(S []map[string]string, A string) float64 { // TODO: A is going to be problematic
-//	return Entropy(S) - (math.Abs(Sv1)/float64(len(S)))*Entropy(Sv1) - (math.Abs(Sv2)/float64(len(S)))*Entropy(Sv2)
-//}
+func Gain(S []map[string]string, A string) float64 {
+
+	/* Gain
+	 * Description: Calculate entropy gain for a given attribute.
+	 * returns: float64, entropy gain value
+	 */
+
+	var Sv1 []map[string]string
+	var Sv2 []map[string]string
+
+	for _ /* index */, entry := range S { //TODO: where do the 2 possible values come from?
+		if entry[A] == "yes" {
+			Sv1 = append(Sv1, entry)
+		} else if entry[A] == "no" {
+			Sv2 = append(Sv2, entry)
+		}
+	}
+	return Entropy(S) - float64(len(Sv1)/len(S))*Entropy(Sv1) - float64(len(Sv2)/len(S))*Entropy(Sv2)
+}
+
+func id3(entries []map[string]string, attributes []string) {
+
+	/* id3
+	 * Description: Main id3 recursive function.
+	 */
+
+	/*
+		If all of the examples belong to the same category, then return a leaf node labeled with that category.
+		  If there are no more attributes, return a leaf node labeled with the most common category in the examples.
+		  otherwise,
+		      select the attribute that results in the greatest information gain
+		      create (and eventually return) a non-leaf node that is labeled with that attribute
+		      For each value v of that attribute:
+		          create a child for that value by applying one of the following two options:
+		              If there are no examples with the value v, then the child is a leaf labeled with the most common category in the current examples
+		              otherwise, the child is the result of running ID3 recursively with the examples that have value v and all the remaining attributes
+	*/
+}
 
 func main() {
 	var training []map[string]string
 	var testing []map[string]string
 
+	//fmt.Println(Entropy(append(training, testing...))) // ... is to append the slice
+
 	training, testing = readDataSet("data/tennis.txt")
-	//
-	//fmt.Println("Training entropy: ", Entropy(training))
-	//fmt.Println("Testing entropy: ", Entropy(testing))
-
-	fmt.Println(Entropy(append(training, testing...))) // ... is to
-
-	//for i, line := range training {
-	//	fmt.Println(i, line)
-	//}
-	//
-	//fmt.Println("---------------")
-	//
-	//for i, line := range testing {
-	//	fmt.Println(i, line)
-	//}
+	fmt.Println(Entropy(append(training, testing...)))
+	fmt.Println("Training entropy: ", Entropy(training))
+	fmt.Println("Testing entropy: ", Entropy(testing))
 }
+
+//TODO: Sometimes training entropy is NaN because the RNG can set one array to totally empty.
